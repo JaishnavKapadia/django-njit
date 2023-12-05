66d8d7b2523e6ce9cfa50dd786557b8c1bb191e1
Akaash Thawani <akaashthawani123@gmail.com>
2023-12-05 17:22:20 -0500

    my changes

--- Campus_Navigator/navigation/models.py
+++ Campus_Navigator/navigation/models.py
@@ -9,3 +9,23 @@ class Location(models.Model):
 
     def __str__(self):
         return f"{self.from_location} to {self.to_location} at {self.timestamp}"
+    
+class SearchLocation(models.Model):
+    TRAVEL_MODE_CHOICES = [
+        ('WALKING', 'Walking'),
+        ('DRIVING', 'Driving'),
+        ('CYCLING', 'Cycling'),
+    ]
+    from_location_name = models.CharField(max_length=100)
+    from_location_lat = models.FloatField()
+    from_location_lng = models.FloatField()
+    to_location_name = models.CharField(max_length=100)
+    to_location_lat = models.FloatField()
+    to_location_lng = models.FloatField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    travel_mode = models.CharField(max_length=10, choices=TRAVEL_MODE_CHOICES)
+    search_by = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f"{self.from_location_name} to {self.to_location_name} at {self.timestamp}"
+    
--- Campus_Navigator/navigation/views.py
+++ Campus_Navigator/navigation/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 from navigation.forms import LocationForm
 from django.shortcuts import render, redirect
-from .models import Location
+from .models import Location, SearchLocation
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
@@ -14,22 +14,49 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def get_locations(request):
     locations = Location.objects.all()
-    data = [{'from_location': location.from_location, 'to_location': location.to_location, 'timestamp': location.timestamp} for location in locations]
+    data = [{'from_location': location.from_location, 'to_location': location.to_location,
+             'timestamp': location.timestamp} for location in locations]
     return JsonResponse(data, safe=False)
-    
+
+
 @csrf_exempt
 def add_location(request):
     if request.method == 'POST':
-        print("API CALL RECICVED")
+        print("API CALL RECEIVED")
         data = json.loads(request.body)
-        from_location = data.get('from_location')
-        to_location = data.get('to_location')
+        print("data",data)
+        # Access data from the payload
+        from_location_data = data.get('from_location', {})
+        to_location_data = data.get('to_location', {})
+        travel_mode = data.get('travel_mode')
+        search_by = data.get('search_by')
 
-        if from_location and to_location:
-            location = Location(from_location=from_location, to_location=to_location)
+        from_location_name = from_location_data.get('name')
+        from_location_lat = from_location_data.get('lat')
+        from_location_lng = from_location_data.get('lng')
+
+        to_location_name = to_location_data.get('name')
+        to_location_lat = to_location_data.get('lat')
+        to_location_lng = to_location_data.get('lng')
+
+        if (
+            from_location_name and from_location_lat and from_location_lng and
+            to_location_name and to_location_lat and to_location_lng
+        ):
+            location = SearchLocation(
+                from_location_name=from_location_name,
+                from_location_lat=float(from_location_lat),
+                from_location_lng=float(from_location_lng),
+                to_location_name=to_location_name,
+                to_location_lat=float(to_location_lat),
+                to_location_lng=float(to_location_lng),
+                travel_mode=travel_mode,
+                search_by=search_by
+            )
+            print("location", location)
             location.save()
-            return JsonResponse({'message': 'Location added successfully','status':'success'})
+            return JsonResponse({'message': 'Location added successfully', 'status': 'success'})
         else:
-            return JsonResponse({'error': 'Invalid data provided','status':'fail'})
+            return JsonResponse({'error': 'Invalid data provided', 'status': 'fail'})
     else:
-        return JsonResponse({'error': 'Invalid request method','status':'fail'})
+        return JsonResponse({'error': 'Invalid request method', 'status': 'fail'})
